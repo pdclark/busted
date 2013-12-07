2b02f573f8261bb4075d616dbeabb25f09eb3644
Paul Clark <pdclark@brainstormmedia.com>
2013-12-07 00:55:10 -0900

    Version bump

--- busted.php
+++ busted.php
@@ -3,7 +3,7 @@
 Plugin Name: Busted!
 Plugin URI: http://github.com/brainstormmedia/busted
 Description: Force browsers to load the most recent file if modified. Requires <a href="http://codex.wordpress.org/Function_Reference/wp_enqueue_style">wp_enqueue_style</a>, <a href="http://codex.wordpress.org/Function_Reference/wp_enqueue_script">wp_enqueue_script</a>, or <a href="http://codex.wordpress.org/Function_Reference/get_stylesheet_uri">get_stylesheet_uri</a> be used to load scripts and styles.
-Version: 1.1
+Version: 1.2
 Author: Brainstorm Media
 Author URI: http://brainstormmedia.com
 License: GPLv2
